db3e0e7993cd33f5ba8ce501e1204212b1cf96cf
atharva <atharva.jamdar@iqdigi.com>
2026-02-07 16:37:58 +0530

    Hosted link changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://yashpandav.dev',
+            url: 'https://atharva-jamdar.vercel.app/',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
